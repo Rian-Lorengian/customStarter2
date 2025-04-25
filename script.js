78f613fe378e212c3e721b1edcfbe4a553609395
Rian-Lorengian <rianlorengian@gmail.com>
2025-04-25 16:36:31 -0300

    Finalizado data, e saudacao

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ function formatarHorario(hora, minuto) {
     return `${hora_formatada}:${minuto_formatado}`
 }
 
+function formatarData(dia_da_semana, dia_do_mes, mes, ano) {
+    return `${dia_da_semana}, ${dia_do_mes} de ${mes} de ${ano}`
+}
 
 // Definição das váriavies utilitárias de dados para o script
 let dados_horario = {}
@@ -26,7 +29,10 @@ let dados_data = {}
 
 //elementos
 const diasDaSemana = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"]
+const meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
 const relogio = document.getElementById('hora')
+const dia = document.getElementById('dia')  
+const saudacao_campo = document.getElementById("salute")
 
 // Funções startup
 function inicio() {
@@ -43,7 +49,7 @@ function getDataAtual() {
     incrementaDadoHora('hora', dados_hora_atual.getHours())
     incrementaDadoData('dia_da_semana', diasDaSemana[dados_hora_atual.getDay()])
     incrementaDadoData('dia_do_mes', dados_hora_atual.getDate())
-    incrementaDadoData('mes', dados_hora_atual.getMonth())
+    incrementaDadoData('mes', meses[dados_hora_atual.getMonth()])
     incrementaDadoData('ano', dados_hora_atual.getFullYear())
 }
 
@@ -70,8 +76,9 @@ function processarMinuto() {
     atualizaRelogio()
 }
 
-function processarHora() {
+async function processarHora() {
     atualizaData()
+    atualizarSaudacao()
 }
 
 //Funções para atualizar o relógio da tela
@@ -84,5 +91,30 @@ function atualizaRelogio() {
 
 //Funções para atualizar a data
 function atualizaData() {
+    if(dados_data['dia_da_semana'] && dados_data['dia_do_mes'] && dados_data['mes'] && dados_data['ano']) {
+        const data_formatada = formatarData(dados_data['dia_da_semana'], dados_data['dia_do_mes'], dados_data['mes'], dados_data['ano'])
+        dia.innerText = data_formatada
+    }
+}
 
+//Funções para atualizar a saudação
+function atualizarSaudacao() {
+    if (!(dados_horario.minuto && dados_horario.hora)) return;
+
+    const horas = dados_horario['hora']
+    let saudacao = ''
+    const user_name = 'Rian'
+
+    
+    if (horas >= 6 && horas <= 12)  {
+        saudacao = `Bom dia, ${user_name}`;
+    }
+    else if (horas >= 13 && horas <= 18)  {
+        saudacao = `Boa tarde, ${user_name}`;
+    }
+    else if ((horas >= 19 && horas <= 23) || (horas >= 0 && horas <= 5)) {
+        saudacao = `Boa noite, ${user_name}`;
+    }
+
+    saudacao_campo.innerText = saudacao
 }
